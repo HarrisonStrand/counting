9a299a3ef187b0c8fdd6f35c88ff62fc9f4a0599
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-27 15:35:21 -0700

    add alerts for 0 and negative numbers
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,16 @@ $(document).ready(function() {
     const number2 = $("input#countBy").val();
     const countToInt = parseInt(number1);
     const countByInt = parseInt(number2);
+    
+    if (countToInt <= -1 && countByInt <= -1) {
+      alert("Please enter positive number");
+    }
+    else if (countByInt === 0 && countByInt === 0) {
+      alert("Please enter a numerical value");
+    }
 
     for (let currentNumber = 0; currentNumber <= countToInt; currentNumber += countByInt) {
       $("#result").append(" " + currentNumber);
     }
-    
-
   });
 });
