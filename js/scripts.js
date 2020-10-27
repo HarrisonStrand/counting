166e140a8bcd4e74191321c1608f6ba8da9e8724
HarrisonStrand <harrisonstrand@gmail.com>
2020-10-27 13:17:58 -0700

    add centered header to HTML
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,10 @@ $(document).ready(function() {
     const countToInt = parseInt(number1);
     const countByInt = parseInt(number2);
 
-    // let total = 0;
     for (let currentNumber = 0; currentNumber <= countToInt; currentNumber += countByInt) {
-      // total += currentNumber;
       $("#result").append(" " + currentNumber);
     }
     
 
   });
 });
-
-
-
-// for (let index = 1; index <= number1; index += number2) {
-//   console.log(index)
